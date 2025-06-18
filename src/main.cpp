109fc1c1db99d6359aef69604a1a2dcca0b98d49
joseph <tannv.k55v@gmail.com>
2025-06-18 12:11:07 +0700

    mac update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-int main() {
+int main()
+{
     std::cout << "Hello, CMake on Windows VSCode! 123" << std::endl;
+    std::cout << "Hello, CMake on Mac VSCode! 123" << std::endl;
     return 0;
 }
